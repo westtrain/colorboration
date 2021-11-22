723e1be0cac6924dc0a3c40a5a9bff504b7509ff
westtrain <it.joshualee@gmail.com>
2021-11-22 09:41:56 +0900

    Edit getAllPalettes Endpoint 앤드포인트가 all이면 모든 앤드포인트를 받아오는 문제가 생김

--- server/router/palettes.js
+++ server/router/palettes.js
@@ -4,7 +4,7 @@ const isAuth = require("../middleware/verifyToken");
 
 const router = express.Router();
 // 엔드포인트 접근시 로그인 확인 절차 필요 IsAuth함수를 미들웨어에서 가져다 쓰면 어떨까 생각중...로그아웃 처럼
-router.get("/all", palettesController.getAllPalettes); // 모든 팔레트 조회
+router.get("/entire", palettesController.getAllPalettes); // 모든 팔레트 조회
 router.get("/random", palettesController.getRandom); // 팔레트들을 랜덤으로 조회
 router.get("/", palettesController.getUserPalette); // 특정 유저가 만든 팔레트 모음 조회
 router.get("/ranking", palettesController.getRanking); // 팔레트의 좋아요 순으로 내림차순으로 조회
